02016b7d7981e3803adab0b0e110c3b492a47175
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-01-23 19:53:01 -0700

    task 1 complete, video feed

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -1,26 +1,38 @@
-#include <stdio.h>
+//#include <stdio.h> // from OpenCV introduction, not needed for this
 #include <opencv2/opencv.hpp>
-#include <iostream>
+//#include <iostream> // not needed
 
 using namespace cv;
-using namespace std;
+//using namespace std; // needed for cout/endl (not needed if std:: is used)
 
 int main(int argc, char** argv )
 {
-  // provided code
   VideoCapture video(0);    // get a camera object
+
+  // attempt video access, and print relevant information to screen
   if (!video.isOpened())
   {
-    cout << "cannot open the video cam" << endl;
+    std::cout << "cannot access video" << std::endl;
     return -1;
   }
+  std::cout << "video access successful" << std::endl;
+  double imgW = video.get(CAP_PROP_FRAME_WIDTH);  //get width of the video
+  double imgH = video.get(CAP_PROP_FRAME_HEIGHT); //get height of the video
+  std::cout << "video dimensions : " << imgW << " x " << imgH << std::endl;
 
-  Mat frame;                // allocate an image buffer object
+
+  Mat frame;                  // allocate an image buffer object
   namedWindow("Millard", CV_WINDOW_AUTOSIZE); // initialize a display window
-  video >> frame;           // grab a frame from the video feed
-  imshow("Millard", frame); // display the grabbed frame
 
-  waitKey(0);
+
+  // perform loop of video feed updates
+  while (1)
+  {
+    video >> frame;           // grab a frame from the video feed
+    imshow("Millard", frame); // display the grabbed frame
+    waitKey(1);               // wait for a few ms
+  }
+
 
   return 0;
 }
