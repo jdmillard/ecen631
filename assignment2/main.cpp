b926e41e1749680744f8bd024e8f6c5baff6d3a8
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-02-08 16:30:32 -0700

    vectors of vectors

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -8,6 +8,8 @@ int main(int argc, char** argv )
 {
   // set save bool
   bool saving = true;
+  int h = 10; // horizontal internal points
+  int v = 7;  // vertical internal points
 
 
   // ------------------------------ TASK 1 ------------------------------
@@ -32,7 +34,7 @@ int main(int argc, char** argv )
   cvtColor(image, image_gray, CV_BGR2GRAY);
 
   // find the chessboard corners
-  Size patternsize(10,7);
+  Size patternsize(h,v);
   std::vector<Point2f> centers;
   bool found;
   found = findChessboardCorners(image_gray, patternsize, centers);
@@ -68,7 +70,32 @@ int main(int argc, char** argv )
 
   // ------------------------------ TASK 2 ------------------------------
   // create OpenCV gui window
-  namedWindow("Task 2", CV_WINDOW_AUTOSIZE); // initialize a display window
+  //namedWindow("Task 2", CV_WINDOW_AUTOSIZE); // initialize a display window
+
+  // initialize vector of vectors of 2-D floating points
+  std::vector<std::vector<Point2f>> img_points;
+  std::vector<std::vector<Point3f>> obj_points;
+  std::vector<Point3f> obj_set;
+  Point3f p_cur;
+
+  // obj_set is set of world frame checker points (0,0,0), (1,0,0) ... (n,m,0)
+  // each image will have a corresponding one, but they are all identical
+  // based on the way the calibrate camera defines the world frames
+
+  // create a vector of 3d points corresponding
+  for (int x=0; x<h; x++)
+  {
+    for (int y=0; y<v; y++)
+    {
+      // generate the current point
+      p_cur.x = x;
+      p_cur.y = y;
+      p_cur.z = 0.0f;
+
+      obj_set.push_back(p_cur);
+    }
+  }
+
 
   // cycle through each calibration image
   std::string n_img_str;
@@ -95,31 +122,33 @@ int main(int argc, char** argv )
     // refine corner locations with subpixel accuracy
     cornerSubPix(image_gray, centers, Size(5,5), Size(-1,-1), TermCriteria(3,30,0.1));
 
+    // add the resulting vector of points to the overall vector
+    img_points.push_back(centers);
+    obj_points.push_back(obj_set);
+
     // draw the corners in color on the original image
     drawChessboardCorners(image, patternsize, Mat(centers), found);
 
     // display the current image
-    imshow("Task 2", image);
-
-
-    // wait for a new key input
-    key = waitKey();
-    if (key == 110)
-    {
-      // the 'n' (next) key was pressed, increment image
-    }
-    else if (key == 27)
-    {
-      // the 'esc' key was pressed, end application
-      std::cout << "terminating" << std::endl;
-      return -1;
-    }
-
-
-
+    //imshow("Task 2", image);
   }
 
 
+  // now use the overall vectors to get the camera calibration
+
+
+  // wait for a new key input
+  key = waitKey();
+  if (key == 110)
+  {
+    // the 'n' (next) key was pressed, increment image
+  }
+  else if (key == 27)
+  {
+    // the 'esc' key was pressed, end application
+    std::cout << "terminating" << std::endl;
+    return -1;
+  }
 
 
 
