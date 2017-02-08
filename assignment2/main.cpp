b549739b6bb5515edccded86e8573abacb616728
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-02-08 14:51:30 -0700

    task 1 complete

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv )
 {
   // ------------------------------ TASK 1 ------------------------------
   // initialize the image object
-  Mat image;
+  Mat image, image_gray;
   // create OpenCV gui window
   namedWindow("Task 1", CV_WINDOW_AUTOSIZE); // initialize a display window
 
@@ -23,6 +23,25 @@ int main(int argc, char** argv )
       printf("No image data \n");
       return -1;
   }
+
+  // convert image format to grayscale
+  cvtColor(image, image_gray, CV_BGR2GRAY);
+
+
+  // find the chessboard corners
+  Size patternsize(10,7);
+  std::vector<Point2f> centers;
+  bool found;
+  found = findChessboardCorners(image_gray, patternsize, centers);
+
+  // refine corner locations with subpixel accuracy
+  cornerSubPix(image_gray, centers, Size(5,5), Size(-1,-1), TermCriteria(3,30,0.1));
+
+  // draw the corners in color on the original image
+  drawChessboardCorners(image, patternsize, Mat(centers), found);
+
+  // save image
+  imwrite( "task1.jpg", image );
 
   // display image in an opencv window
   imshow("Task 1", image);
