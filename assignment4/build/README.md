4a81d5023743417a81dd4ff9b719d56095f9e2ee
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-03-01 10:25:29 -0700

    starting assignment 4

---
+++ assignment4/build/README.md
@@ -0,0 +1,15 @@
+Build and Run
+---
+This assignment performs the trajectory generation of a ball using stereo vision.
+
+
+From the build directory, these commands should build and run a given assignment:
+```
+$ cmake .. && make    # build
+$ ./assignment        # run
+```
+
+Notes
+---
+ * The images are not committed
+ * IDEA: put relevant images in dropbox with dl links here
---
+++ assignment4/main.cpp
@@ -0,0 +1,30 @@
+#include <opencv2/opencv.hpp>
+#include <string> // needed for setting std::strings and using to_string
+#include <fstream>
+
+using namespace cv;
+
+
+int main(int argc, char** argv )
+{
+
+
+////////    ///     //////  //    //       //
+   //      // //   //    // //   //      ////
+   //     //   //  //       //  //         //
+   //    //     //  //////  /////          //
+   //    /////////       // //  //         //
+   //    //     // //    // //   //        //
+   //    //     //  //////  //    //     //////
+
+
+  // initialize variables
+
+  std::cout << "test basic" << std::endl;
+
+
+  
+
+  printf("finished \n");
+  return 0;
+}
