92c11ff7dedea6c01a5c66625ec4d49c7231febb
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-03-01 10:55:48 -0700

    task 1 corners

--- assignment4/main.cpp
+++ assignment4/main.cpp
@@ -19,8 +19,19 @@ int main(int argc, char** argv )
 
 
   // initialize variables and windows
-  std::string   path;
-  Mat           image_left, image_right;
+
+  int                   h = 10; // horizontal internal points
+  int                   v = 7;  // vertical internal points
+
+  bool                  found;
+
+  std::string           path;
+  std::vector<Point2f>  centers_left;
+  std::vector<Point2f>  centers_right;
+
+  Size                  patternsize(h,v);
+  Mat                   image_left, image_right,
+                        image_left_gray, image_right_gray;
 
   namedWindow("Task 1 Left", CV_WINDOW_AUTOSIZE);
   namedWindow("Task 1 Right", CV_WINDOW_AUTOSIZE);
@@ -36,6 +47,24 @@ int main(int argc, char** argv )
   // display left and right images
   imshow("Task 1 Left", image_left);
   imshow("Task 1 Right", image_right);
+
+  // convert images format to grayscale
+  cvtColor(image_left, image_left_gray, CV_BGR2GRAY);
+  cvtColor(image_right, image_right_gray, CV_BGR2GRAY);
+  // find the chessboard corners
+  found = findChessboardCorners(image_left_gray, patternsize, centers_left);
+  found = findChessboardCorners(image_left_gray, patternsize, centers_right);
+  // refine corner locations with subpixel accuracy
+  cornerSubPix(image_left_gray,  centers_left,  Size(5,5), Size(-1,-1), TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 60, 0.001));
+  cornerSubPix(image_right_gray, centers_right, Size(5,5), Size(-1,-1), TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 60, 0.001));
+
+
+
+  // undistortPoints() to undistort and rectify the 4 outermost points
+  // perspectiveTransform() to calculate 3D information of 4 points
+  // add circles to the 4 points on the image pairs & x,y,z information
+
+  // in the writeup, put explanation describing the difference between key functions - above and beyond
 
   // wait for a new key input
   int key = waitKey();
