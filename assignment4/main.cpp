515d2aed24c93f5870842a04a4c6f8e516831216
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-03-01 10:39:51 -0700

    reads images

--- assignment4/main.cpp
+++ assignment4/main.cpp
@@ -18,12 +18,38 @@ int main(int argc, char** argv )
    //    //     //  //////  //    //     //////
 
 
-  // initialize variables
+  // initialize variables and windows
+  std::string   path;
+  Mat           image_left, image_right;
 
-  std::cout << "test basic" << std::endl;
+  namedWindow("Task 1 Left", CV_WINDOW_AUTOSIZE);
+  namedWindow("Task 1 Right", CV_WINDOW_AUTOSIZE);
+  moveWindow("Task 1 Left", 50, 50);
+  moveWindow("Task 1 Right", 700, 50);
 
+  // get left and right images
+  path = "../images/checker/stereoL18.bmp";
+  image_left = imread( path, 1 );
+  path = "../images/checker/stereoR18.bmp";
+  image_right = imread( path, 1 );
 
-  
+  // display left and right images
+  imshow("Task 1 Left", image_left);
+  imshow("Task 1 Right", image_right);
+
+  // wait for a new key input
+  int key = waitKey();
+  if (key == 110)
+  {
+    // the 'n' (next) key was pressed
+  }
+  else if (key == 27)
+  {
+    // the 'esc' key was pressed, end application
+    std::cout << "terminating" << std::endl;
+    return -1;
+  }
+
 
   printf("finished \n");
   return 0;
